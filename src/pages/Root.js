eca0ec8befe6af8db6fe1b0a44ae595e634002c2
notyogesh <yogeshreo@gmail.com>
2023-03-26 17:10:55 +0530

    omg

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,12 +1,12 @@
 import React, { useState, useMemo } from "react";
 import Navbar from "../main/navbar/NavBar";
 import Divider from "@mui/material/Divider";
+import { Box } from "@mui/material";
 
 import { customTheme } from "../utils/Theme";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Outlet } from "react-router-dom";
 import Footer from "../main/Footer";
-// import { Box } from "@mui/system";
 
 const Root = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
@@ -25,11 +25,11 @@ const Root = () => {
   return (
     <div className="root">
       <ThemeProvider theme={theme}>
-        <div className="content">
+        <Box sx={{ flexGrow: 1, minheight: "100dvh" }}>
           <Navbar onClick={changeTheme} />
           <Divider />
           <Outlet />
-        </div>
+        </Box>
 
         <Divider />
         <Footer />
