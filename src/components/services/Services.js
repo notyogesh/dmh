e27730b0b96356cd6c98279399331a8b80bac11f
notyogesh <yogeshreo@gmail.com>
2023-06-06 17:44:58 +0530

    acmv

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -12,7 +12,7 @@ const Services = () => {
       to: "/construction",
     },
     {
-      head: "ACMV",
+      head: "AirConditioning & Mechanical Vendilation System",
       body: "(Air Conditioning and Mechanical Ventilation System) Heating, ventilation, and air conditioning  services,We offer a comprehensive range of HVAC services, including installation, maintenance, and repairs. ",
       img: "https://img.freepik.com/free-vector/air-conditioner-repair-repairman-with-tools_33099-272.jpg?size=626&ext=jpg&ga=GA1.1.2136575466.1684650788&semt=ais",
       to: "/airconditioning&mechanical-vendilation-system",
